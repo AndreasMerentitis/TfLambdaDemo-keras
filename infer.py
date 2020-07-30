01bbe01269bc341c3902f9e09ff7c737ad734af9
Andreas <andreas.merentitis@gmail.com>
2020-07-30 12:41:07 +0200

    update infer file

--- infer.py
+++ infer.py
@@ -11,11 +11,65 @@ import boto3
 import tensorflow as tf
 import numpy as np
 
+import pandas as pd
+from keras import models
+from sklearn.preprocessing import LabelEncoder
+import re
+
 import census_data
 
-FILE_DIR = '/tmp/'
-BUCKET = os.environ['BUCKET']
+from subprocess import check_output
 
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+FILE_DIR = '/tmp/'
+S3_MODEL_DIR = ''
+
+#BUCKET = os.environ['BUCKET']
+BUCKET = 'serverless-ml-1'
+
+
+def _easy_input_function_keras(data_dict):
+    transform_needed = [False,
+               True,
+               False,
+               True,
+               False,
+               True,
+               True,
+               True,
+               True,
+               True,
+               False,
+               False,
+               False,
+               True,
+               True]
+    
+    columns = ['age', 'workclass', 'fnlwgt', 'education', 'education_num',
+       'marital_status', 'occupation', 'relationship', 'race', 'gender',
+       'capital_gain', 'capital_loss', 'hours_per_week', 'native_country']
+               
+    df = pd.DataFrame(data_dict.items())[1]
+    full_data = pd.DataFrame.from_records(df.values)
+    full_data = full_data.T
+    data = np.zeros(shape=(full_data.shape[0], full_data.shape[1]), dtype=np.float32)
+
+    for i in range(len(transform_needed)):
+        if transform_needed[i]:
+            tmp_data = full_data.iloc[:, i].tolist()
+            encoder = LabelEncoder()
+            encoder.fit(tmp_data)
+            data[:, i] = encoder.transform(tmp_data)
+        else:
+            data[:, i] = full_data.iloc[:, i].tolist()
+
+
+    x_predict = data
+    return x_predict
 
 def _easy_input_function(data_dict, batch_size=64):
     """
@@ -42,50 +96,58 @@ def _easy_input_function(data_dict, batch_size=64):
 
 
 def inferHandler(event, context):
+    out = str(check_output(["df", '-k']))
+    result = re.search(r"(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\%\s+\/tmp", out)
+    size, used, available, used_pct = (int(g) for g in result.groups())  
+    logging.warning('available space value is %s', available)
+
     body = json.loads(event.get('body'))
+    
+    #logging.warning('body value is %s', body)
 
     # Read in prediction data as dictionary
     # Keys should match _CSV_COLUMNS, values should be lists
     predict_input = body['input']
+    
+    logging.warning('predict_input value is %s', predict_input)
 
     # Read in epoch
     epoch_files = body['epoch']
+    
+    logging.warning('epoch_files value is %s', epoch_files)
+    
+    logging.warning('BUCKET value is %s', BUCKET)
+
+    s3_full_model_path = os.path.join(S3_MODEL_DIR,'model.tar.gz')
+    logging.warning('model_path value is %s', s3_full_model_path)
+    
+    logging.warning('BUCKET value is %s', BUCKET)
 
     # Download model from S3 and extract
     boto3.Session(
         ).resource('s3'
         ).Bucket(BUCKET
-        ).download_file(
-            os.path.join(epoch_files,'model.tar.gz'),
-            FILE_DIR+'model.tar.gz')
+        ).download_file(s3_full_model_path,'/tmp/model.tar.gz')
 
     tarfile.open(FILE_DIR+'model.tar.gz', 'r').extractall(FILE_DIR)
+    
+    custom_model_dir=os.path.join(FILE_DIR+'model_ML.h5')
+    
+    x_test = _easy_input_function_keras(predict_input)
+    print (x_test)
 
-    # Create feature columns
-    wide_cols, deep_cols = census_data.build_model_columns()
-
-    # Load model
-    classifier = tf.estimator.LinearClassifier(
-                    feature_columns=wide_cols,
-                    model_dir=FILE_DIR+'tmp/model_'+epoch_files+'/',
-                    warm_start_from=FILE_DIR+'tmp/model_'+epoch_files+'/')
-
-    # Setup prediction
-    predict_iter = classifier.predict(
-                        lambda:_easy_input_function(predict_input))
-
-    # Iterate over prediction and convert to lists
-    predictions = []
-    for prediction in predict_iter:
-        for key in prediction:
-            prediction[key] = prediction[key].tolist()
-
-        predictions.append(prediction)
+    # make predictions from the h5 file after loading with keras
+    h5_model = models.load_model(custom_model_dir)
+    x_test_casted = np.reshape(x_test[0,:13],(13,1))
+    h5_predictions = h5_model.predict(x_test_casted.T)
+    print (h5_predictions)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(predictions,
+        "body": json.dumps(h5_predictions.tolist(),
                             default=lambda x: x.decode('utf-8'))
     }
+    
+    logging.warning('response value is %s', response)
 
     return response
